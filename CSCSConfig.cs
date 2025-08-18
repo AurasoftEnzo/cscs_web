db7bd4322efef0297863ac52ca23896c719e6770
EnzoGrubisa <aurasoft.enzo@gmail.com>
2025-08-18 15:29:24 +0200

    Update CSCSConfig.cs

--- CSCSConfig.cs
+++ CSCSConfig.cs
@@ -5,10 +5,10 @@
         public string SQLConnectionString { get; set; }
         public string ScriptsDirectory { get; set; }
         public string StartScript { get; set; }
-        public string PreprocessSripts { get; set; }
 
         public string PreprocessTokens { get; set; }
-
         public string TemplatesDirectory { get; set; }
+
+        //public string PreprocessSripts { get; set; }
     }
 }
